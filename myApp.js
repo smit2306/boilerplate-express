83a0215717bc9d9af7971b1c167a09c976b70cdf
smit2306 <psmit29@gmail.com>
2020-11-29 13:28:50 +0530

    Added code for second exercise.

--- myApp.js
+++ myApp.js
@@ -5,5 +5,11 @@ var app = express();
 // first exercise
 console.log('Hello World');
 
+// second exercise
+app.get('/', (req, res) => {
+    console.log('This is the req param: ', req);
+    res.send('Hello Express');
+});
+
 // module exports
 module.exports = app;
